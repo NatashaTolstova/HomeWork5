9da16d28696964aaa59675e9f5eb6c5c1476f7a7
NATASHA <klimknatasha@yandex.ru>
2024-11-12 00:12:05 +0300

    1

--- 1.c
+++ 1.c
@@ -14,3 +14,48 @@
 Данные на выходе: Одно целое число. Количество зерен на данной
 клетке.
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef unsigned long long int ullint;
+
+int enterCellNumber()
+{
+    int number;
+    scanf("%d", &number);
+    return number;
+}
+
+bool checkNumber(int cellNumber)
+{
+    return ((cellNumber >= 1) && (cellNumber <= 64));
+}
+
+ullint getRiceGrainsNumber(int cellNumber)
+{
+    ullint riceNumber = 1;
+    for (int i = 1; i < cellNumber; i++)
+    {
+        riceNumber *= 2;
+    }
+    return riceNumber;
+}
+
+int main(void)
+{
+    int cell = enterCellNumber();
+
+    if (checkNumber(cell))
+    {
+        ullint number = getRiceGrainsNumber(cell);
+        printf("%llu", number);
+    }
+    else
+    {
+        printf("wrong number");
+        return 0;
+    }
+        
+    return 0;
+}
