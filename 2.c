fe6964d3d8969d0bb0ba6da0c8b1a6e9648bfc25
NATASHA <klimknatasha@yandex.ru>
2024-11-12 00:24:35 +0300

    2

--- 2.c
+++ 2.c
@@ -6,3 +6,22 @@ int nod(int a, int b)
 
 Данные на входе: Два целых положительных числа
 Данные на выходе: Одно целое число - наибольший общий делитель.*/
+
+#include <stdio.h>
+
+int nod(int a, int b)
+{
+    while (a != b)
+    {
+      (a > b) ? (a -= b) : (b -= a); 
+    }
+    return a;
+}
+
+int main(void)
+{
+    int numberA, numberB;
+    scanf("%d %d", &numberA, &numberB);
+    printf("%d", nod(numberA, numberB));
+    return 0;
+}
