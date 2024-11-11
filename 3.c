57336b64d65fcfddbb92bf86f7d4a406dfdbed3b
NATASHA <klimknatasha@yandex.ru>
2024-11-12 00:30:38 +0300

    3

--- 3.c
+++ 3.c
@@ -5,3 +5,26 @@ int is_happy_number(int n)
 
 Данные на входе: Целое не отрицательное число
 Данные на выходе: YES или NO*/
+
+#include <stdio.h>
+
+int is_happy_number(int n)
+{
+    int sum = 0, mult = 1, result;
+    while (n > 0)
+    {
+        sum += n % 10;
+        mult *= n % 10;
+        n /= 10;
+    }
+    (sum == mult) ? (result = 1) : (result = 0);
+    return result;
+}
+
+int main(int argc, char const *argv[])
+{
+    int number;
+    scanf("%d", &number);
+    is_happy_number(number) ? printf("YES") : printf("NO");
+    return 0;
+}
